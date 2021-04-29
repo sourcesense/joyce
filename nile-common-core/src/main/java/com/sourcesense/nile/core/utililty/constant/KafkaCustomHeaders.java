287f9ac67d35270960769ce3f1dca074baf52953
niko.usai <niko.usai@sourcesense.com>
2021-04-29 13:24:24 +0200

    parent schema handling

--- nile-common-core/src/main/java/com/sourcesense/nile/core/utililty/constant/KafkaCustomHeaders.java
+++ nile-common-core/src/main/java/com/sourcesense/nile/core/utililty/constant/KafkaCustomHeaders.java
@@ -4,4 +4,5 @@ public class KafkaCustomHeaders {
 
     public static final String MESSAGE_ACTION = "X-Nile-Action";
     public static final String INGESTION_SCHEMA = "X-Nile-Schema";
+    public static final String COLLECTION = "X-Nile-collection";
 }
