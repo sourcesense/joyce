8e03533f847a9d2d0fd6dfeea8d6fe4704138588
rinant <a.rinaldi@live.it>
2021-07-06 16:19:16 +0200

    Test fixes

---
+++ joyce-schema-engine/src/test/java/com/sourcesense/joyce/schemaengine/configuration/SchemaTransformerConfigTest.java
@@ -0,0 +1,126 @@
+package com.sourcesense.joyce.schemaengine.configuration;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sourcesense.joyce.schemaengine.annotation.SchemaTransformationHandler;
+import com.sourcesense.joyce.schemaengine.exception.InvalidHandlerKeywordException;
+import com.sourcesense.joyce.schemaengine.exception.JoyceSchemaEngineException;
+import com.sourcesense.joyce.schemaengine.handler.FixedValueTransformerHandler;
+import com.sourcesense.joyce.schemaengine.handler.SchemaTransformerHandler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class SchemaTransformerConfigTest {
+
+	@Test
+	void shouldLoadNoHandler() {
+		ApplicationContext context = mock(ApplicationContext.class);
+		Map<String, Object> customHandlers = Collections.emptyMap();
+		when(context.getBeansWithAnnotation(SchemaTransformationHandler.class)).thenReturn(customHandlers);
+		SchemaTransformerConfig schemaTransformerConfig = new SchemaTransformerConfig(context);
+
+		Map<String, SchemaTransformerHandler> actual = schemaTransformerConfig.transformerHandlers();
+		Map<String, SchemaTransformerHandler> expected = Collections.emptyMap();
+
+		assertThat(actual.equals(expected));
+	}
+
+	@Test
+	void shouldLoadCustomHandler() {
+		ApplicationContext context = mock(ApplicationContext.class);
+		FixedValueTransformerHandler transformerHandler = new FixedValueTransformerHandler();
+		Map<String, Object> customHandlers = Map.of(
+				"fixedValueTransformerHandler", transformerHandler
+		);
+		when(context.getBeansWithAnnotation(SchemaTransformationHandler.class)).thenReturn(customHandlers);
+		SchemaTransformerConfig schemaTransformerConfig = new SchemaTransformerConfig(context);
+
+		Map<String, SchemaTransformerHandler> actual = schemaTransformerConfig.transformerHandlers();
+		Map<String, SchemaTransformerHandler> expected = Map.of("$fixed", transformerHandler);
+
+		assertThat(actual.equals(expected));
+	}
+
+	@Test
+	void shouldAddDollarSignIfMissing() {
+		ApplicationContext context = mock(ApplicationContext.class);
+		TestNoDollarTransformerHandler transformerHandler = new TestNoDollarTransformerHandler();
+		Map<String, Object> customHandlers = Map.of(
+				"testNoDollarTransformerHandler", transformerHandler
+		);
+		when(context.getBeansWithAnnotation(SchemaTransformationHandler.class)).thenReturn(customHandlers);
+		SchemaTransformerConfig schemaTransformerConfig = new SchemaTransformerConfig(context);
+
+		Map<String, SchemaTransformerHandler> actual = schemaTransformerConfig.transformerHandlers();
+		Map<String, SchemaTransformerHandler> expected = Map.of("$test", transformerHandler);
+
+		assertThat(actual.equals(expected));
+	}
+
+	@Test
+	void shouldNotLoadHandlerNotImplementingSchemaTransformerHandler() {
+		ApplicationContext context = mock(ApplicationContext.class);
+		TestNotImplementingSchemaTransformerHandler transformerHandler = new TestNotImplementingSchemaTransformerHandler();
+		Map<String, Object> customHandlers = Map.of(
+				"testNotImplementingSchemaTransformerHandler", transformerHandler
+		);
+		when(context.getBeansWithAnnotation(SchemaTransformationHandler.class)).thenReturn(customHandlers);
+		SchemaTransformerConfig schemaTransformerConfig = new SchemaTransformerConfig(context);
+
+		assertThrows(
+				JoyceSchemaEngineException.class,
+				schemaTransformerConfig::transformerHandlers
+		);
+	}
+
+	@Test
+	void shouldNotLoadEmptyKeywordTransformerHandler() {
+		ApplicationContext context = mock(ApplicationContext.class);
+		TestEmptyKeywordTransformerHandler transformerHandler = new TestEmptyKeywordTransformerHandler();
+		Map<String, Object> customHandlers = Map.of(
+				"testEmptyKeywordTransformerHandler", transformerHandler
+		);
+		when(context.getBeansWithAnnotation(SchemaTransformationHandler.class)).thenReturn(customHandlers);
+		SchemaTransformerConfig schemaTransformerConfig = new SchemaTransformerConfig(context);
+
+		assertThrows(
+				InvalidHandlerKeywordException.class,
+				schemaTransformerConfig::transformerHandlers
+		);
+	}
+
+	@SchemaTransformationHandler(keyword = "notImplementing")
+	private static class TestNotImplementingSchemaTransformerHandler {
+	}
+
+	@SchemaTransformationHandler
+	private static class TestEmptyKeywordTransformerHandler implements SchemaTransformerHandler {
+		@Override
+		public JsonNode process(String key, JsonNode value, JsonNode source, Optional<JsonNode> metadata, Optional<Object> context) {
+			return null;
+		}
+	}
+
+	@SchemaTransformationHandler(keyword = "test")
+	private static class TestNoDollarTransformerHandler implements SchemaTransformerHandler {
+
+		@Override
+		public JsonNode process(String key, JsonNode value, JsonNode source, Optional<JsonNode> metadata, Optional<Object> context) {
+			return null;
+		}
+	}
+}
--- joyce-schema-engine/src/test/java/com/sourcesense/joyce/schemaengine/service/SchemaEngineTest.java
+++ joyce-schema-engine/src/test/java/com/sourcesense/joyce/schemaengine/service/SchemaEngineTest.java
@@ -17,152 +17,136 @@
 package com.sourcesense.joyce.schemaengine.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.TextNode;
-import com.sourcesense.joyce.schemaengine.exception.InvalidSchemaException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchemaException;
+import com.sourcesense.joyce.schemaengine.ResourceLoader;
 import com.sourcesense.joyce.schemaengine.exception.JoyceSchemaEngineException;
+import com.sourcesense.joyce.schemaengine.handler.SchemaTransformerHandler;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
-    import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
-public class SchemaEngineTest {
+public class SchemaEngineTest implements ResourceLoader {
 
-//    protected Path loadResource(String name) throws URISyntaxException {
-//        URL res = this.getClass().getClassLoader().getResource(name);
-//        return Paths.get(res.toURI());
-//    }
+
+	private ObjectMapper mapper;
+
+	@BeforeEach
+	void init() {
+		mapper = new ObjectMapper();
+	}
+
+//  Todo: Invalid Metaschema
+//	@Test
+//	void dummyParserShouldBeRegisteredCorrectly() throws URISyntaxException, IOException {
+//		String schema = Files.readString(loadResource("schema/10.json"));
+//		String source = Files.readString(loadResource("source/10.json"));
+//		SchemaTransformerHandler dummyHandler = mock(SchemaTransformerHandler.class);
+//		when(dummyHandler.process(any(), any(), any(), any(), any())).thenReturn(new TextNode("foobar"));
+//		Map<String, SchemaTransformerHandler> handlers = Map.of(
+//				"$path", dummyHandler,
+//				"$fixed", dummyHandler,
+//				"$meta", dummyHandler
+//		);
+//		SchemaEngine schemaEngine = new SchemaEngine(mapper, handlers);
+//		JsonNode result = schemaEngine.process(schema, source);
+//		Assertions.assertEquals("Leanne Graham", result.get("name").asText());
+//		Assertions.assertEquals("foobar", result.get("mail").asText());
+//		Assertions.assertEquals("foobar", result.get("address").asText());
+//	}
+
+	@Test
+	void invalidSourceShouldThrow() throws URISyntaxException, IOException {
+		String schema = Files.readString(loadResource("schema/10.json"));
+		String source = Files.readString(loadResource("source/11.json"));
+		SchemaTransformerHandler jsonPathTransformerHandler = mock(SchemaTransformerHandler.class);
+		Map<String, SchemaTransformerHandler> handlers = Map.of("$path", jsonPathTransformerHandler);
+		SchemaEngine schemaEngine = new SchemaEngine(mapper, handlers);
+
+		Assertions.assertThrows(
+				JsonSchemaException.class,
+				() -> schemaEngine.process(schema, source)
+		);
+	}
+
+	//Todo: Invalid Metaschema
+//	@Test
+//	void handlersShouldBeAppliedCascading() throws URISyntaxException, IOException {
+//		String schema = Files.readString(loadResource("schema/20.json"));
+//		String source = Files.readString(loadResource("source/10.json"));
 //
-//    @Mock
-//    SchemaEngineProperties props;
+//		SchemaTransformerHandler handler = mock(SchemaTransformerHandler.class);
+//		when(handler.process(any(), eq(new TextNode("$.email")), any(), any(), any()))
+//				.thenReturn(new TextNode("mario"));
+//		when(handler.process(any(), eq(new TextNode("uppercase")), eq(new TextNode("mario")), any(), any()))
+//				.thenReturn(new TextNode("MARIO"));
 //
-//    @Test
-//    void registerHandlerKeyWithoutDollarShouldAddDollarSign() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/10.json"));
-//        String source = Files.readString(loadResource("source/10.json"));
+//		Map<String, SchemaTransformerHandler> handlers = Map.of(
+//				"$path", handler,
+//				"$transform", handler
+//		);
 //
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        TransormerHandler jsonPathTransformerHandler = Mockito.mock(TransormerHandler.class);
-//        Mockito.when(jsonPathTransformerHandler.process(any(), any(), any(), any(), any()))
-//                .thenReturn(new TextNode("foobar"));
-//        schemaEngine.registerHandler("path", jsonPathTransformerHandler);
-//        schemaEngine.registerMetaSchema();
-//        JsonNode result = schemaEngine.process(schema, source);
-//        Assertions.assertEquals("Leanne Graham", result.get("name").asText());
-//        Assertions.assertEquals("foobar", result.get("mail").asText());
-//        Assertions.assertEquals("foobar", result.get("address").asText());
-//    }
+//		SchemaEngine schemaEngine = new SchemaEngine(mapper, handlers);
 //
-//
-//    @Test
-//    void dummyParserShouldBeRegisteredCorrectly() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/10.json"));
-//        String source = Files.readString(loadResource("source/10.json"));
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        TransormerHandler jsonPathTransformerHandler = Mockito.mock(TransormerHandler.class);
-//        Mockito.when(jsonPathTransformerHandler.process(any(), any(), any(), any(), any()))
-//                .thenReturn(new TextNode("foobar"));
-//        schemaEngine.registerHandler("$path", jsonPathTransformerHandler);
-//        schemaEngine.registerHandler("$fixed", jsonPathTransformerHandler);
-//        schemaEngine.registerHandler("$meta", jsonPathTransformerHandler);
-//        schemaEngine.registerMetaSchema();
-//        JsonNode result = schemaEngine.process(schema, source);
-//        Assertions.assertEquals("Leanne Graham", result.get("name").asText());
-//        Assertions.assertEquals("foobar", result.get("mail").asText());
-//        Assertions.assertEquals("foobar", result.get("address").asText());
-//    }
-//
-//    @Test
-//    void invalidSourceShouldThrow() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/10.json"));
-//        String source = Files.readString(loadResource("source/11.json"));
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        TransormerHandler jsonPathTransformerHandler = Mockito.mock(TransormerHandler.class);
-//
-//        schemaEngine.registerHandler("$path", jsonPathTransformerHandler);
-//        schemaEngine.registerMetaSchema();
-//        InvalidSchemaException exc = Assertions.assertThrows(
-//                InvalidSchemaException.class,
-//                () -> schemaEngine.process(schema, source)
-//        );
-//    }
-//
-//    @Test
-//    void handlersShouldBeAppliedCascading() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/20.json"));
-//        String source = Files.readString(loadResource("source/10.json"));
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//
-//        TransormerHandler handler = Mockito.mock(TransormerHandler.class);
-//        Mockito.when(handler.process(any(), eq(new TextNode("$.email")), any(), any(), any()))
-//                .thenReturn(new TextNode("mario"));
-//
-//        Mockito.when(handler.process(any(), eq(new TextNode("uppercase")), eq(new TextNode("mario")), any(), any()))
-//                .thenReturn(new TextNode("MARIO"));
-//
-//        schemaEngine.registerHandler("$path", handler);
-//        schemaEngine.registerHandler("$transform", handler);
-//        schemaEngine.registerMetaSchema();
-//        JsonNode result = schemaEngine.process(schema, source);
-//
-//        Assertions.assertEquals("MARIO", result.get("name").asText());
-//
-//    }
-//
-//    @Test
-//    void addingFieldShouldNotBreakChanges() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/11.json"));
-//        String newSchema = Files.readString(loadResource("schema/12.json"));
-//        ;
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        Boolean ret = schemaEngine.checkForBreakingChanges(schema, newSchema);
-//        Assertions.assertFalse(ret);
-//    }
-//
-//    @Test
-//    void deprecatingFieldShouldBreakChanges() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/12.json"));
-//        String newSchema = Files.readString(loadResource("schema/13.json"));
-//        ;
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        Boolean ret = schemaEngine.checkForBreakingChanges(schema, newSchema);
-//        Assertions.assertTrue(ret);
-//    }
-//
-//    @Test
-//    void changingTypeShouldThrow() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/13.json"));
-//        String newSchema = Files.readString(loadResource("schema/14.json"));
-//        ;
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        Assertions.assertThrows(JoyceSchemaEngineException.class, () -> {
-//            schemaEngine.checkForBreakingChanges(schema, newSchema);
-//        });
-//    }
-//
-//    @Test
-//    void changingTypeExtendingTypesShouldNotThrowsAndDoNotBreaks() throws URISyntaxException, IOException {
-//        String schema = Files.readString(loadResource("schema/14.json"));
-//        String newSchema = Files.readString(loadResource("schema/15.json"));
-//        ;
-//        SchemaEngine schemaEngine = new SchemaEngine(props);
-//        Assertions.assertFalse(schemaEngine.checkForBreakingChanges(schema, newSchema));
-//    }
-//
-//
+//		String expected = "MARIO";
+//		String actual = schemaEngine.process(schema, source).get("name").asText();
+//		Assertions.assertEquals(expected, actual);
+//	}
+
+	@Test
+	void addingFieldShouldNotBreakChanges() throws URISyntaxException, IOException {
+		JsonNode schema = this.readNodeFromResource("schema/11.json");
+		JsonNode newSchema = this.readNodeFromResource("schema/12.json");
+		SchemaEngine schemaEngine = new SchemaEngine(mapper, new HashMap<>());
+		Boolean ret = schemaEngine.checkForBreakingChanges(schema, newSchema);
+		Assertions.assertFalse(ret);
+	}
+
+	@Test
+	void deprecatingFieldShouldBreakChanges() throws URISyntaxException, IOException {
+		JsonNode schema = this.readNodeFromResource("schema/12.json");
+		JsonNode newSchema = this.readNodeFromResource("schema/13.json");
+		SchemaEngine schemaEngine = new SchemaEngine(mapper, new HashMap<>());
+		Boolean ret = schemaEngine.checkForBreakingChanges(schema, newSchema);
+		Assertions.assertTrue(ret);
+	}
+
+	@Test
+	void changingTypeShouldThrow() throws URISyntaxException, IOException {
+		JsonNode schema = this.readNodeFromResource("schema/13.json");
+		JsonNode newSchema = this.readNodeFromResource("schema/14.json");
+		SchemaEngine schemaEngine = new SchemaEngine(mapper, new HashMap<>());
+		Assertions.assertThrows(JoyceSchemaEngineException.class, () -> {
+			schemaEngine.checkForBreakingChanges(schema, newSchema);
+		});
+	}
+
+	@Test
+	void changingTypeExtendingTypesShouldNotThrowsAndDoNotBreaks() throws URISyntaxException, IOException {
+		JsonNode schema = this.readNodeFromResource("schema/14.json");
+		JsonNode newSchema = this.readNodeFromResource("schema/15.json");
+		SchemaEngine schemaEngine = new SchemaEngine(mapper, new HashMap<>());
+		Assertions.assertFalse(schemaEngine.checkForBreakingChanges(schema, newSchema));
+	}
+
+	private JsonNode readNodeFromResource(String resource) throws URISyntaxException, IOException {
+		Path resourcePath = this.loadResource(resource);
+		String resourceJson = Files.readString(resourcePath);
+		return mapper.readValue(resourceJson, JsonNode.class);
+	}
 }
